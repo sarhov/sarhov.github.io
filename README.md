ade191afc5683f52642ac4f9fcb358ea66908f96
sarhov <sarhov@sarhov.com>
2014-11-11 02:13:19 +0400

    add basic files and markup, navigation links, favicons

---
+++ README.md
@@ -0,0 +1,1 @@
+the source of sarhov.com
--- config.rb
+++ config.rb
@@ -5,11 +5,12 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
-javascripts_dir = "js"
-
+javascripts_dir = "js" 
+relative_assets = true
+line_comments = false
+# output_style = :compressed
+# sourcemap = true
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
