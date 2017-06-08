ce395cf528d9c67ee88d33353a1d483ea7611c48
sarhov <sarhov@sarhov.com>
2017-06-08 10:42:31 +0400

    added new scroll button

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function($) {
   
+  // Scroll to
+  $(function() {
+  	$('a.jsScrollBottom').on('click', function(e) {
+  		e.preventDefault();
+  		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  	});
+  });
 });
