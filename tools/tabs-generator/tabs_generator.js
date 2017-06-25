c51553228cd9649db0c07c19b010375a7477abc4
sarhov <sarhov@sarhov.com>
2017-06-25 13:56:57 +0400

    added remove function as well

--- tools/tabs-generator/tabs_generator.js
+++ tools/tabs-generator/tabs_generator.js
@@ -3,14 +3,14 @@ var quill = new Quill('#editor', {
 });
 
 var actionAddTab = $('.jsActionAddTab');
-var actionRemoveTab = $('.jsActionRemoveTab');
 var actionTabItem = $('.jsActionTabItem');
+
 var actionTabList = $('.jsActionTabList');
 
 function checkL(){
 	var checkLength = actionTabList.find('.jsActionTabItem').length;
-	console.log(checkLength) 
-	if (checkLength > 4) {
+	// console.log(checkLength) 				
+	if (checkLength > 5) {
 		actionAddTab.hide(); 
 	}
 	else{
@@ -18,16 +18,32 @@ function checkL(){
 	}
 }
 
-function actionTabs(){ 
-	actionAddTab.click(function(event) {
-		event.preventDefault();
-		// console.log(checkLength_)
-		var newPosition = $(this).prev('li');
-		var tabIndex = parseInt(newPosition.attr('data-index')) + 1;
-		var clonedTab = newPosition.clone();
-		clonedTab.attr('data-index', tabIndex).insertAfter(newPosition).find('.jsTitleText').html('Title - ' + tabIndex)
-		checkL();
-	});
-}
+actionAddTab.click(function(event) {
+	event.preventDefault();
+	// console.log(checkLength_)
+	var newPosition = $(this).prev('li');
+	var tabIndex = parseInt(newPosition.attr('data-index')) + 1;
+	var clonedTab = newPosition.clone(true, true);
+	clonedTab.attr('data-index', tabIndex).insertAfter(newPosition).find('.jsTitleText').html('Title - ' + tabIndex)
+	checkL();
+});
 
-actionTabs()
+$('.jsActionRemoveTab').click(function(event) {
+	event.preventDefault();
+	var newPosition = $(this).closest('li');
+	var restItems = newPosition.nextAll('.jsActionTabItem');
+	console.log(restItems)
+	newPosition.remove();
+	restItems.each(function(index, el) {
+		var newEl = $(el);
+		var tabIndex = parseInt(newEl.attr('data-index')) - 1;
+		console.log(tabIndex)
+		newEl.attr('data-index', tabIndex);
+		var checkTitle = newEl.find('.jsTitleText').text()
+		if (checkTitle.indexOf("Title - ") >= 0){
+				newEl.find('.jsTitleText').html('Title - ' + tabIndex)
+		}
+	});
+
+	checkL();
+});
